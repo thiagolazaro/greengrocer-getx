51a2194717969cb6dd53084f1961cdf5f85d7c3b
Thiago Lázaro <thiagolazaro2009@gmail.com>
2022-12-10 21:55:44 -0300

    Navegando para a base

--- lib/src/pages/auth/controller/auth_controller.dart
+++ lib/src/pages/auth/controller/auth_controller.dart
@@ -1,11 +1,17 @@
 import 'package:get/get.dart';
+import 'package:greengrocer/src/models/user_model.dart';
 import 'package:greengrocer/src/pages/auth/repository/auth_repository.dart';
 import 'package:greengrocer/src/pages/auth/result/auth_result.dart';
+import 'package:greengrocer/src/pages_routes/app_pages.dart';
+import 'package:greengrocer/src/services/utils_services.dart';
 
 class AuthController extends GetxController {
   RxBool isLoading = false.obs;
 
   final authRepository = AuthRepository();
+  final utilSsServices = UtilsServices();
+
+  UserModel user = UserModel();
 
   Future<void> signIn({
     required String email,
@@ -20,10 +26,16 @@ class AuthController extends GetxController {
 
     result.when(
       success: (user) {
-        print(user);
+        this.user = user;
+
+        // Navegar para proxima pagina
+        Get.offAllNamed(PagesRoutes.baseRoute);
       },
       error: (message) {
-        print(message);
+        utilSsServices.showToast(
+          message: message,
+          isError: true,
+        );
       },
     );
   }
